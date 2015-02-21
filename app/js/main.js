aa49722ed32342ecfb6965759a0e9a38a4d42f3c
bttf <adnanchowdhury88@gmail.com>
2015-02-21 16:17:01 -0500

    basic pointerLock working

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,23 @@
 (function() {
   var scene, camera, renderer;
   var geometry, material, mesh;
+  var havePointerLock = checkForPointerLock();
+  var controls, controlsEnabled;
 
   init();
   animate();
 
   function init() {
+    initPointerLock();
+
     scene = new THREE.Scene();
     scene.fog = new THREE.Fog(0xffffff, 0, 750);
 
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
     camera.position.y = 10;
-    camera.position.z = 100;
+
+    controls = new THREE.PointerLockControls(camera);
+    scene.add(controls.getObject());
 
     // floor
     scene.add(createFloor());
@@ -35,5 +41,46 @@
     texture.repeat.set(256, 256);
     material = new THREE.MeshBasicMaterial({ color: 0xffffff, map: texture });
     return new THREE.Mesh(geometry, material);
+  }
+
+  function checkForPointerLock() {
+    return 'pointerLockElement' in document || 'mozPointerLockElement' in document || 'webkitPointerLockElement' in document;
+  }
+  
+  function initPointerLock() {
+    var element = document.body;
+    if (havePointerLock) {
+      var pointerlockchange = function (event) {
+        if (document.pointerLockElement === element || 
+            document.mozPointerLockElement === element || 
+            document.webkitPointerLockElement === element) {
+          controlsEnabled = true;
+          controls.enabled = true;
+        } else {
+          controls.enabled = false;
+        }
+      };
+
+      var pointerlockerror = function (event) {
+        element.innerHTML = 'PointerLock Error';
+      };
+
+      document.addEventListener('pointerlockchange', pointerlockchange, false);
+      document.addEventListener('mozpointerlockchange', pointerlockchange, false);
+      document.addEventListener('webkitpointerlockchange', pointerlockchange, false);
+
+      document.addEventListener('pointerlockerror', pointerlockerror, false);
+      document.addEventListener('mozpointerlockerror', pointerlockerror, false);
+      document.addEventListener('webkitpointerlockerror', pointerlockerror, false);
+
+      var requestPointerLock = function(event) {
+        element.requestPointerLock = element.requestPointerLock || element.mozRequestPointerLock || element.webkitRequestPointerLock;
+        element.requestPointerLock();
+      };
+
+      element.addEventListener('click', requestPointerLock, false);
+    } else {
+      element.innerHTML = 'Bad browser; No pointer lock';
+    }
   }
 })();
